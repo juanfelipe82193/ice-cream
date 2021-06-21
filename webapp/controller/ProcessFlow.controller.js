97ddba83a950b371c2e10acf7dfde9c586be5e74
juanfelipe82193 <juan19_93@hotmail.com>
2021-06-20 21:50:12 -0500

    first commit

---
+++ webapp/controller/ProcessFlow.controller.js
@@ -0,0 +1,20 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/base/strings/formatMessage"
+], function (Controller, formatMessage) {
+    "use strict";
+
+    return Controller.extend("sap.suite.ui.commons.demokit.tutorials.icecream.01.controller.ProcessFlow", {
+
+        formatMessage: formatMessage,
+
+        onNavButtonPressed: function () {
+            this.getOwnerComponent().getRouter().navTo("home");
+        },
+
+        getValuesDelta: function (fFirstValue, fSecondValue) {
+            return fSecondValue - fFirstValue;
+        }
+
+    });
+});
